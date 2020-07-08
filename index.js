7ad35724d6d146435c015c1463e2b395e61a1827
Ararat Poghosyan <ararat.poghosyan@gmail.com>
2020-07-08 16:23:03 +0400

    Initial commit

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const { Sequelize } = require('sequelize');
 
-// Create database connection
 const sequelize = new Sequelize('postgres://batman:wrong_secret@localhost:5432/myDB')
-
-sequelize.authenticate().catch(function(errors) { 
-    console.log(errors)
-});
-
-// Setup express application
-
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send("Hello world"))
+sequelize.authenticate()
+    .then(() => {
+        const message = "All good"
+    })
+    .catch(function (errors) {
+        message = "Error while connecting to database"
+    });
+
+app.get('/', (req, res) => res.send(message))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
