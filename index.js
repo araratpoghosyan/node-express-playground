7413d6e6313b28ab41aba1146025bd9f373a3e21
Ararat Poghosyan <ararat.poghosyan@gmail.com>
2020-07-08 16:14:12 +0400

    Add error message

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const { Sequelize } = require('sequelize');
 // Create database connection
 const message = "All good"
 const sequelize = new Sequelize('postgres://batman:wrong_secret@localhost:5432/myDB')
-sequelize.authenticate().then(function(errors) { 
-    message = "Error while connecting to database"
-    console.log(errors) 
+sequelize.authenticate().catch(function(errors) { 
+    message = "Error whic"
+    console.log(errors)
 });
 
 // Setup express application
