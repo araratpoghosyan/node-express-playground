6ea3003fdd758a80453964873f0498c43e616898
Ararat Poghosyan <ararat.poghosyan@gmail.com>
2020-07-08 18:39:30 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const { Sequelize } = require('sequelize');
 
+let message = "All good"
 const sequelize = new Sequelize('postgres://batman:secret@localhost:5432/myDB')
 const app = express()
 const port = 3000
 
 sequelize.authenticate()
-    .then(() => {
-        const message = "All good"
-    })
+    .then(() => {})
     .catch(function (errors) {
         message = "Error while connecting to database"
     });
